da8f011f4e3632c983ec1829d84265eb4788f884
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-26 22:12:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(event) {
+  //Write your code here
+  event.preventDefault();
+  let radius = parseFloat(document.querySelector("#radius").value);
+  let volume = document.querySelector("#volume");
+  if (radius >= 0) {
+    let vol = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    volume.value = vol.toFixed(4);
+  }
+}
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById("MyForm").onsubmit = volume_sphere;
